8f2e14bfb32b6a970ac5198d910c0de30ea64cc2
John <johnwilday@yahoo.com>
2023-05-11 01:26:30 -0600

    Removed file for deployment

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 
 module.exports = () => {
   return {
@@ -42,7 +42,7 @@ module.exports = () => {
           },
         ],
       }),
-      new MiniCssExtractPlugin({filename: 'style.css'}),
+      
       // Injects our custom service worker
       new InjectManifest({
         swSrc: './src-sw.js',
